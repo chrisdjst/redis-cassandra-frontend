2370a322abbc63e8c01ad9a424065e34f3fc5132
christopher <christopher@boxti.com.br>
2022-06-30 00:06:30 -0300

    vai adicionar exceção do host

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
-  lintOnSave:false
+  lintOnSave:false,
+  devServer: {
+    disableHostCheck: true
+  }
 })
